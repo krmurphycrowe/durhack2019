7fc8d61534f4f77238cd706de4b080b45652965b
James Townsend <j.a.townsend2000@gmail.com>
2019-11-23 16:27:12 +0000

    Added makeTweets() function

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import tweepy, json, pull, post, uwu
+import tweepy, json, pull, post, uwu, time
 
 
 if __name__ == '__main__':
     pass
+
+def makeTweets(tweetBodies):
+    i = 0
+    while i < len(tweetBodies):
+        try:
+            post.post(tweetBodies[i])
+            i += 1
+        except tweepy.RateLimitError:
+            time.sleep(60)
