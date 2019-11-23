9bb36ca18d6c83c5565d5dc3dd14c71c45b0445f
Katie-RoseCrowe <katiekatiecrowe@gmail.com>
2019-11-23 20:56:50 +0000

    modified files so keys.json no longer needed

--- post.py
+++ post.py
@@ -1,30 +1,14 @@
-import tweepy, json
+import tweepy, json, os
 
-def load_file(to_load, key):
-    # this is a method to load a file, used in loading the twitter key details file
-    try:
-        with open(to_load + '.json', 'r') as loadFile:
-            # loads file, uses main key
-            return json.load(loadFile)[key]
-
-    except FileNotFoundError:
-        print('File doesn\'t exist.')
-        return 0
-
-    except KeyError:
-        print('File is in an invalid format')
-        return 0
 
 def post(text):
     try:
         # loads json details file and saves in variables
-        filename = 'keys'
-        key = 'DETAILS'
-        file = load_file(filename, key)
-        cKey = file["CONSUMER_KEY"]
-        cSecret = file["CONSUMER_SECRET"]
-        key1 = file["KEY1"]
-        key2 = file["KEY2"]
+
+        cKey = os.environ['cKey']
+        cSecret = os.environ['cSecret']
+        key1 = os.environ['key1']
+        key2 = os.environ['key2']
 
         # creates auth, which is needed to access the api
         auth = tweepy.OAuthHandler(cKey, cSecret)
