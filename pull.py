6ecc7dbfdfcf84a35be7a426cda43ac620c89f04
James Townsend <j.a.townsend2000@gmail.com>
2019-11-23 16:08:55 +0000

    Removed debug info, added docstring and exception checking

--- pull.py
+++ pull.py
@@ -1,15 +1,19 @@
 import tweepy, json
 
 def loadMostRecentTweet():
-    with open("lastTweet") as f:
-        lastID = f.read()
-        return int(lastID)
+    try:
+        with open("lastTweet") as f:
+            lastID = f.read()
+            return int(lastID)
+    except FileNotFoundError:
+        return 0
 
 def saveMostRecentTweet(id):
     with open ("lastTweet", "w") as f:
         f.write(str(id))
 
 def getBowisTweets(tAPI):
+    """Returns list of text bodies of Boris Johnson's twitter feed."""
     tweets = tAPI.user_timeline(user_id=3131144855, since_id=loadMostRecentTweet(), count=20, tweet_mode="extended")
     # Gets up to the 20 most recent tweets from bojo's twitter
     bodies = [] # Creates an empty list to store the bodies (not as suspicious as it sounds)
@@ -27,37 +31,3 @@ def getBowisTweets(tAPI):
         if not body.startswith(".@BorisJohnson"): # If it's not funky retweet
             newbod.append(body) # Append the body
     return newbod[::-1] # Tweepy fetches them most-recent first and we want most-recent last, so we flip the list
-
-def load_file(to_load, key):
-    # this is a method to load a file, used in loading the twitter key details file
-    try:
-        with open(to_load + '.json', 'r') as loadFile:
-            # loads file, uses main key
-            return json.load(loadFile)[key]
-
-    except FileNotFoundError:
-        print('File doesn\'t exist.')
-        return 0
-
-    except KeyError:
-        print('File is in an invalid format')
-        return 0
-
-def test():
-    try:
-        filename = 'keys'
-        key = 'DETAILS'
-        file = load_file(filename, key)
-        cKey = file["CONSUMER_KEY"]
-        cSecret = file["CONSUMER_SECRET"]
-        key1 = file["KEY1"]
-        key2 = file["KEY2"]
-
-        auth = tweepy.OAuthHandler(cKey, cSecret)
-        auth.secure = True
-        auth.set_access_token(key1, key2)
-
-        api = tweepy.API(auth)
-        print(getBowisTweets(api))
-    except tweepy.error.TweepError:
-        pass
