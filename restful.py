728475895bdcbaff056c546c6c194a40a4b13cb1
James Townsend <j.a.townsend2000@gmail.com>
2019-11-24 05:31:07 +0000

    Restful now returns times as well

--- restful.py
+++ restful.py
@@ -23,11 +23,14 @@ def getBowisTweets():
     tweets = tAPI.user_timeline(user_id=3131144855, count=200, tweet_mode="extended")
     # Gets up to the 20 most recent tweets from bojo's twitter
     bodies = [] # Creates an empty list to store the bodies (not as suspicious as it sounds)
+    times = []
     for tweet in tweets: # For every tweet
         try: # So basically tweepy is really dumb and the only way to tell if it's a retweet or not is to use try/except
             bodies.append(tweet.retweeted_status.full_text) # Append the retweeted status
         except AttributeError: # Yada yada tweepy is dumb
             bodies.append(tweet.full_text) # Append the text of the tweet
+        ttime = tweet.created_at
+        times.append(str(ttime.hour) + ":" + str(ttime.minute) + ", " + str(ttime.day) + "/" + str(ttime.month) + "/" + str(ttime.year))
     newbod = [] # Creates a list for the processed bodies (again not as sus as it sounds)
     for body in bodies: # For every tweet body we have
         if not body.startswith(".@BorisJohnson"): # If it's not funky retweet
@@ -35,7 +38,7 @@ def getBowisTweets():
     uwus = []
     for body in newbod:
         uwus.append(uwu.uwuMe(body))
-    return uwus[::-1] # Tweepy fetches them most-recent first and we want most-recent last, so we flip the list
+    return (uwus[::-1], times) # Tweepy fetches them most-recent first and we want most-recent last, so we flip the list
 
 cachedTweets = getBowisTweets()
 cachedTime = datetime.datetime.now()
@@ -49,7 +52,7 @@ def api_id():
     else:
         return "Error: no page specified"
 
-    if datetime.datetime.now().timestamp() - cachedTime.timestamp() > 120:
+    if datetime.datetime.now().timestamp() - cachedTime.timestamp() > 300:
         cachedTweets = getBowisTweets()
         cachedTime = datetime.datetime.now()
 
